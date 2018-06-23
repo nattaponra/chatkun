57ffb5d4e10d28cf6341e91993ff3b62a1098172
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-06-24 01:48:25 +0700

    Send message to service

--- src/Chatkun.php
+++ src/Chatkun.php
@@ -8,19 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatKun extends Model
 {
-    protected $table = "chatkun";
+    protected $table    = "chatkun";
     protected $fillable = ["user_id"];
 
     public function fromUser(){
         return $this->hasOne("App\User","id","user_id");
     }
 
+    private function sendMessageToService($from,$to,$message)
+    {
+        $chatKunFactory  = new ChatKunFactory();
+        $chatKunService  = $chatKunFactory->getChatService(config("chatkun.default_service",""));
+        $chatKunService->sendMessage($from,$to,$message);
+    }
+
     public function sendMessage(User $user,  $message){
+
+        //Send message to service
+        $this->sendMessageToService($this->fromUser->id, $user->id, $message);
+
+        //Save message to database
         $chatKunMessage = ChatKunMessage::create([
             'user_id'    => $this->fromUser->id,
             'to_user_id' => $user->id
         ]);
-
 
         ChatKunSubMessage::create([
             'messages_id'        => $chatKunMessage->id,
@@ -33,7 +44,10 @@ class ChatKun extends Model
 
 
 
-
-
-
 }
+
+
+
+
+
+
