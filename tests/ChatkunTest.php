966f6166283d44fffac92a436faf07842e3b6a34
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-09-01 11:52:43 +0700

    Create new test

--- tests/ChatkunTest.php
+++ tests/ChatkunTest.php
@@ -1,20 +1,56 @@
 <?php
-require_once __DIR__ . '/../app/autoload.php';
 
-use nattaponra\chatkun\Chatkun;
-use nattaponra\chatkun\services\PusherChatkun;
+namespace nattaponra\chatkun\tests;
+
+use App\User;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
 use PHPUnit\Framework\TestCase;
 
 
-class chatkunTest extends TestCase
+class ChatkunTest extends TestCase
 {
+    use InteractsWithDatabase;
 
+    public function testCreateUser1(){
 
-    public function testSend()
-    {
-        $chatkun = new Chatkun(new PusherChatkun("71baa59665e9ba7ac6e9", "344751", "8efbc5d481a62566ace8", "ap1"));
-        $result = $chatkun->sendMessage("1", "2", "Hello");
-        print_r($result);
+        $user1 = User::create([
+            "name"=>"user1",
+            "email"=>"user1@chatkun.com",
+            "password"=>"11111111",
+
+        ]);
+
+//        $this->assertDatabaseHas("users",[
+//            "email" => "user1@chatkun.com"
+//        ]);
+        return $user1;
     }
+
+    public function testCreateUser2(){
+
+        $user2 = User::create([
+            "name"=>"user2",
+            "email"=>"user2@chatkun.com",
+            "password"=>"22222222",
+
+        ]);
+
+//        $this->assertDatabaseHas("users",[
+//            "email" => "user2@chatkun.com"
+//        ]);
+
+        return $user2;
+    }
+
+
+    /**
+     * @depends testCreateUser1
+     * @depends testCreateUser2
+     */
+    public function testClearData($user1,$user2){
+//        $user1->delete();
+//        $user2->delete();
+    }
+
 
 }
